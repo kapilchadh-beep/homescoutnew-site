388ae8ab2cfb00a7c6f6b04be44103108fb043e0
Kapil Chadha <kapilchadh@gmail.com>
2026-01-02 15:00:11 +1300

    Sync local changes

---
+++ server/src/components/ui/toaster.tsx
@@ -0,0 +1,19 @@
+﻿import * as React from "react"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+export function Toaster() {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  )
+}
